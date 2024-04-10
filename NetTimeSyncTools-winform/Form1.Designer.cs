e2bf9851559da6d06e1faf98ae940b03d4480507
王徐鹏 <2840772674@qq.com>
2024-04-10 21:48:25 +0800

    添加项目文件。

---
+++ NetTimeSyncTools-winform/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace NetTimeSyncTools_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 101);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 284);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 391);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(151, 391);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 34);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(290, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 34);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(516, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 34);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(655, 391);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(133, 34);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,46 @@
+# About this Reposity
+
+This repositry is for network time protocal to sync your time, which used C#. 
+
+We used .NET Framework 4.6.2 to build this Application.
+
+You can download this application source code and compile it. This Solution is built with Visual Studio 2022.
+
+# Supported OS
+
+Windows OS which installed or running .NET Framework 4.6.2 or greater.
+
+P.S. That Means this Application is supported the following Operating System:
+- Windows Vista Service Pack 2*
+- Windows 7 Service Pack 1*
+- Windows 8.1*
+- Windows 10 Version 1507 or later
+- Windows 10 2015 Long-Term Service Branch
+- Windows 10 2016 Long-Term Service Branch
+- Windows 10 Long-Term Service Channel 2019
+- Windows 10 Long-Term Service Channel 2021
+- Windows 11 Version 21H2 or later
+- Windows Server 2008 Service Pack 2*
+- Windows Server 2008 R2 Service Pack 1*
+- Windows Server 2012*
+- Windows Server 2012 R2*
+- Windows Server 2016
+- Windows Server 2019
+- Windows Server 2022
+
+Linux or Mac OS is not have support plan.
+
+*Need install .NET Framework 4.6.2 Mannually or check the updates.
+
+# Future Timeline or event
+Because Windows 10 2015 Long-Term Service Branch is supported until October 14th, 2025, this reposity must use .NET Framework 4.6.2 to support this operating system.
+
+So after October 14th, 2025, which means Windows 10 2015 Long-Term Service Branch ended its lifecycle. This reposity will migrate to latest .NET Platform, which supported Windows 10 Build 14393(That means Windows 10 2016 Long-Term Service Branch, Windows Server 2016 or greater). 
+
+After January 12th, 2027, which means Windows Server 2016 ended its lifecycle. There might have some supported OS changes. Before that day, we will show some changings. After that day, Our Program will migrate to Win UI 3
+
+# About Linux supporting
+
+Linux branch might be built after this reposity launched its first stable release.
+
+And After October 14th, 2025, we might merge linux Branch and Windows Branch, which use C# and .NET Platform.
